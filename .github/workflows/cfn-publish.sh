e858f0e688bdb2cd1be3a68858a2b0cda3febe74
govardhan <govardhan.pagidi@mongodb.com>
2023-09-04 13:55:58 -0500

    multiregion

--- .github/workflows/cfn-publish.sh
+++ .github/workflows/cfn-publish.sh
@@ -86,6 +86,9 @@ for ResourceName in "${ResourceNames[@]}"; do
     elif [[ "$ResourceName" == "third-party-integration" ]];  then
           echo "third-party-integration"
     fi
+    if [ -z ${OTHER_PARAMS+x} ];then
+        echo "OTHER_PARAMS param is empty"
+    fi
 
     Path="cfn-resources/${ResourceName}/"
     CodeBuild_Project_Name="${ResourceName}-project-$((1 + RANDOM % 1000))"
