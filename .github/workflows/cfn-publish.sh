2e058a428006f55dd49909186ef46da15d551e3b
govardhan <govardhan.pagidi@mongodb.com>
2023-09-04 13:48:20 -0500

    multiregion

--- .github/workflows/cfn-publish.sh
+++ .github/workflows/cfn-publish.sh
@@ -48,22 +48,41 @@ read -ra ResourceNames <<< "$RESOURCES"
 
 # iterate over the array of resources
 for ResourceName in "${ResourceNames[@]}"; do
-    echo "resource: $ResourceName"
+      echo "resource: $ResourceName"
 
-    # Currently OTHER_PARAMS is only used for
-    # 1. federated-settings-org-role-mapping
-    # 2. trigger
-    # 3. ldap-configuration
-    # 4. ldap-verify
-    # 5. thirdpartyintegrations
+      # Currently OTHER_PARAMS is only used for
+      # 1. federated-settings-org-role-mapping
+      # 2. trigger
+      # 3. ldap-configuration
+      # 4. ldap-verify
+      # 5. thirdpartyintegrations
 
-    # if condition for string comparison
-    if [[ "$ResourceName" == "federated-settings-org-role-mapping" ]]; then
-        OTHER_PARAMS='{"OrgRoleMapping":[{"OrgID":"${ATLAS_ORG_ID}","OrgRole":"ORG_MEMBER","AWSRoleArn":"arn:aws:iam::711489243244:role/DevOpsIntegrationsContractorsSSM"}]}'
-    elif [[ "$ResourceName" == "trigger" ]]; then
-        OTHER_PARAMS='{"Trigger":[{"OrgID":"${ATLAS_ORG_ID}","TriggerName":"${RESOURCE_NAME}","ClusterName":"${RESOURCE_NAME}","ClusterType":"REPLICASET","ClusterTier":"M10","ClusterProvider":"AWS","ClusterRegion":"US_EAST_1","ClusterVersion":"4.4","ClusterBackupEnabled":"true","ClusterDiskSizeGB":"10","ClusterNumShards":"1","ClusterReplicationFactor":"3","ClusterLabels":"{\"environment\":\"dev\"}","ClusterAutoScalingComputeEnabled":"true","ClusterAutoScalingComputeMaxInstanceSize":"M10","ClusterAutoScalingComputeMinInstanceSize":"M10","ClusterAutoScalingDiskGBEnabled":"true","ClusterAutoScalingDiskGBMaxSize":"100","ClusterAutoScalingDiskGBMinSize":"10","ClusterAutoScalingIOPSMax":"1000","ClusterAutoScalingIOPSMin":"1000","ClusterAutoScalingIOPSMode":"PROVISIONED","ClusterAutoScalingIOPSPerGB":"0.1","ClusterAutoScalingIOPSPerGBMax":"0.1","ClusterAutoScalingIOPSPerGBMin":"0.1","ClusterAutoScalingInstanceType":"M10","ClusterAutoScalingIsEnabled":"true","ClusterAutoScalingIsProvisioned":"true","ClusterAutoScalingProviderName":"AWS","ClusterAutoScalingProviderRegion":"US_EAST_1","ClusterAutoScalingProviderType":"REPLICASET","ClusterAutoScalingProviderVersion":"4.4","ClusterAutoScalingStorageType":"PROVISIONED","ClusterAutoScalingVolumeType":"STANDARD"}]}'
-    elif [[ "$ResourceName" == "ldap-configuration" ]]; then
-        OTHER_PARAMS='{"LDAPConfiguration":[{"OrgID":"${ATLAS_ORG_ID}","BindPassword":"${ATLAS_LDAP_BIND_PASSWORD}","BindUsername":"${ATLAS_LDAP_BIND_USERNAME}","ConnectionTimeoutSecs":"30","ConnectTimeoutSecs":"30","FollowReferrals":"false","GroupMemberIdAttribute":"member","GroupNameAttribute"}]}'
+     # if condition for string comparison
+     if [[ "$ResourceName" == "trigger" ]]; then
+          echo "trigger"
+          cat "$(dirname "$0")"/scripts/ldap-configuration.json
+          # fill the LDAP_BIND_PASSWORD in scripts/ldap-configuration.json file using jq
+          jq --arg ATLAS_FEDERATED_SETTINGS_ID "${ATLAS_FEDERATED_SETTINGS_ID}" \
+             '.ATLAS_FEDERATED_SETTINGS_ID |= $ATLAS_FEDERATED_SETTINGS_ID' \
+              "$(dirname "$0")/scripts/federated-settings-org-role-mapping.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/scripts/federated-settings-org-role-mapping.json"
+           OTHER_PARAMS=$(cat "$(dirname "$0")"/scripts/federated-settings-org-role-mapping-temp.json)
+     elif [[ "$ResourceName" == "federated-settings-org-role-mapping" ]]; then
+          echo "federated-settings-org-role-mapping"
+     elif [[ "$ResourceName" == "ldap-configuration" ]]; then
+          echo "ldap-configuration"
+     elif [[ "$ResourceName" == "ldap-verify" ]]; then
+          echo "ldap-configuration"
+          cat "$(dirname "$0")"/scripts/ldap-configuration.json
+          # fill the LDAP_BIND_PASSWORD in scripts/ldap-configuration.json file using jq
+          jq --arg LDAP_BIND_PASSWORD "${LDAP_BIND_PASSWORD}" \
+             --arg LDAP_BIND_USER_NAME "${LDAP_BIND_USER_NAME}" \
+             --arg LDAP_HOST_NAME "${LDAP_HOST_NAME}" \
+             '.LDAP_BIND_PASSWORD |= $LDAP_BIND_PASSWORD | .LDAP_BIND_USER_NAME |= $LDAP_BIND_USER_NAME | .LDAP_HOST_NAME |= $LDAP_HOST_NAME' \
+              "$(dirname "$0")/scripts/ldap-configuration.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/scripts/ldap-configuration-temp.json"
+
+          OTHER_PARAMS=$(cat "$(dirname "$0")"/scripts/ldap-configuration-temp.json)
+          # convert json to string
+          #OtherParamsString=$(echo "${OTHER_PARAMS}" | jq -r '.[0]')
     fi
 
     Path="cfn-resources/${ResourceName}/"
@@ -77,7 +96,7 @@ for ResourceName in "${ResourceNames[@]}"; do
         .[0].TargetLocationMaxConcurrency?|=$TargetLocationsMaxConcurrency |
         .[0].Accounts[0]?|=$AccountIds |
         .[0].Regions?|=($Regions | gsub(" "; "") | split(",")) ' \
-        "$(dirname "$0")/locations.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/locations-temp.json"
+        "$(dirname "$0")/scripts/locations.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/locations-temp.json"
 
 
     jq --arg Repository "${Repository}" \
@@ -104,9 +123,10 @@ for ResourceName in "${ResourceNames[@]}"; do
       .BuilderRole[0]?|=$BuilderRole |
       .AssumeRole[0]?|=$AssumeRole |
       .LogDeliveryBucket[0]?|=$LogDeliveryBucket ' \
-      "$(dirname "$0")/params.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/params-temp.json"
+      "$(dirname "$0")/scripts/params.json" >tmp.$$.json && mv tmp.$$.json "$(dirname "$0")/params-temp.json"
 
     params_json_content=$(cat "$(dirname "$0")"/params-temp.json)
+
     locations_json_content=$(cat "$(dirname "$0")"/locations-temp.json)
 
     # use the aws cli to start the automation execution
@@ -116,6 +136,5 @@ for ResourceName in "${ResourceNames[@]}"; do
         --parameters "${params_json_content}" \
         --target-locations "${locations_json_content}" \
         --region "${Document_Region}"
-
 done
 
